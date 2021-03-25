1c49b35256fdf0caa7bba81b28757de7bf547b25
aulik <80682655+aulik@users.noreply.github.com>
2021-03-25 23:12:16 +0100

    Update app.py
    kodkommentarer. ..

--- app.py
+++ app.py
@@ -15,13 +15,15 @@ searchterm =  form.getvalue('searchbox')
 
 
 
-api_key = '18983e10f268a5371158020929510bfe'
-base_url = 'https://api.themoviedb.org/3/'+'search/movie?api_key='+api_key+'&query='+'fight'
-search_URL = 'https://api.themoviedb.org/3/'+'search/movie?api_key='+api_key+'&query='
+api_key = '18983e10f268a5371158020929510bfe' # Vår authentication key. 
+base_url = 'https://api.themoviedb.org/3/'+'search/movie?api_key='+api_key+'&query='+'fight' #temporär url endast för test. 
+search_URL = 'https://api.themoviedb.org/3/'+'search/movie?api_key='+api_key+'&query=' #Sök URL för vårt API. 
 
+
+'''Hämtar användarens input från sökfunktionen i index.html för att sedan göra en sökning med hjälp av API. '''
 
 @app.route('/app')
-def index2():
+def search_function(): 
 	search = request.args.get('searchbox')
 	ssl._create_default_https_context =  ssl._create_unverified_context
 	search = urllib.parse.quote(search)
@@ -29,8 +31,10 @@ def index2():
 	json_data = json.loads(conn.read())
 	return render_template('index.html', data=json_data['results'])
 
+''' Temporär startsida för att spike-testa API'''
+
 @app.route('/')
-def index():
+def index(): #temporär test sida för att pröva API
 	ssl._create_default_https_context =  ssl._create_unverified_context
 	conn = requests.urlopen(base_url)
 	json_data = json.loads(conn.read())
